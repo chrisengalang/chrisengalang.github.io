50f9b0ddaa803c1a679d35be168f16ab74524839
Chrisen Galang <chrisen.galang@gmail.com>
2023-05-25 11:11:23 +0800

    Add Sidebar, Navigation and Profile layout

--- src/component/Sidebar/Sidebar.tsx
+++ src/component/Sidebar/Sidebar.tsx
@@ -1,7 +1,16 @@
+import Navigation from "./Navigation";
+import Profile from "./Profile";
 import "./sidebar.css";
 
 const Sidebar = () => {
-  return <div className="col-3 vh-100 sidebar"></div>;
+  return (
+    <div className="col-3 vh-100 sidebar">
+      <div className="sidebar-container">
+        <Profile />
+        <Navigation />
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
